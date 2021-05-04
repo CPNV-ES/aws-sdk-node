474fc8692d2329d7e694edd685a580e74d9d2bc7
PEREIRA Gabriel <Gabriel.PEREIRA@cpnv.ch>
2021-05-04 14:41:35 +0200

    load environment variables from .env

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
-import { S3 } from "aws-sdk";
+import EC2Client from "aws-sdk/clients/ec2";
+import * as dotenv from "dotenv";
 
-console.log("Hello there !");
+dotenv.config();
+
+const ec2client = new EC2Client({ region: process.env.REGION });
+
+ec2client.createVpc((err, data) => {
+    console.log(err);
+    console.log(data);
+});
