9648e96e6b971bff31bf6292158c4ba6503467ef
Alexandre Philibert <philibert.alexandre@gmail.com>
2021-05-25 14:55:41 +0200

    Remove unused AwsVpcManager from main.ts

--- src/main.ts
+++
@@ -1,5 +0,0 @@
-import { AwsVpcManager } from "./classes/AwsVpcManager";
-import { config } from "./config";
-
-const aws = new AwsVpcManager("", config.AWS_REGION);
-aws.createVpc("test-vpc-coucou", "10.0.0.0/16");
