ef4b1372d693bc112a6cb53fb2f00e2da26d9827
Alexandre Philibert <philibert.alexandre@gmail.com>
2021-06-01 13:52:44 +0200

    Import all classes in index file

---
+++ src/classes/index.ts
@@ -0,0 +1,4 @@
+import { AwsInternetGateway } from "./AwsInternetGateway";
+import { AwsRouteTableManager } from "./AwsRouteTableManager";
+import { AwsSubnetManager } from "./AwsSubnetManager";
+import { AwsVpcManager } from "./AwsVpcManager";
